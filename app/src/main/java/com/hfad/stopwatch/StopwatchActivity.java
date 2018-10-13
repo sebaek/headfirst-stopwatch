f314dfdb135a81e505ba024c4f565f55a2533a07
sebaek <sebaek@gmail.com>
2018-10-13 21:09:46 +0900

    implements onStart, onStop methods

--- app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
+++ app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
@@ -13,6 +13,7 @@ public class StopwatchActivity extends Activity {
 
     private int seconds = 0;
     private boolean running;
+    private boolean wasRunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +23,24 @@ public class StopwatchActivity extends Activity {
         if (savedInstanceState != null) {
             seconds = savedInstanceState.getInt("seconds");
             running = savedInstanceState.getBoolean("running");
+            wasRunning = savedInstanceState.getBoolean("wasRunning");
         }
         runTimer();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        wasRunning = running;
+        running = false;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (wasRunning) {
+            running = true;
+        }
     }
 
     @Override
@@ -31,6 +48,7 @@ public class StopwatchActivity extends Activity {
         super.onSaveInstanceState(savedInstanceState);
         savedInstanceState.putInt("seconds", seconds);
         savedInstanceState.putBoolean("running", running);
+        savedInstanceState.putBoolean("wasRunning", wasRunning);
     }
 
     public void onClickStart(View view) {
